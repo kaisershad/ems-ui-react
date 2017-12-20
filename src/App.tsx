ca0f16651b13da1074f107ca31de643d5f372837
kshad <kaiser.anwar.shad@zalando.de>
2017-12-20 12:26:34 +0100

    5 feat: overview filter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import './App.css';
 import Header from './header/header';
 import Navigator from './navigator/navigator';
+import Overview from './overview/overview';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
-        <Navigator/>
+        <Header />
+        <Navigator />
+        <Overview />
       </div>
     );
   }
---
+++ src/overview/overview.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Row, Col, Icon, Menu, Dropdown } from 'antd';
+
+const menu = (
+  <Menu>
+    <Menu.Item key="1">Label1</ Menu.Item>
+    <Menu.Item key="2">Label2</ Menu.Item>
+    <Menu.Item key="3">Label3</ Menu.Item>
+  </ Menu>
+);
+
+function Overview () {
+  return (
+    <div>
+      <Row type="flex" gutter={16} justify="end">
+        <Col span={2}>
+          <Icon type="filter" />
+          Filter
+        </ Col>
+        <Col span={4}>
+          <Dropdown overlay={menu}>
+            <p>
+              group by type <Icon type="down" />
+            </p>
+          </ Dropdown>
+        </ Col>
+      </ Row>
+    </ div>
+  );
+}
+
+export default Overview;
